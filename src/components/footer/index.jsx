d21f345f6d159b59c29c591d96b59df091c7a665
wkauan <kauanmbomfim5@gmail.com>
2024-08-01 22:14:17 -0300

    feat: Adicionando footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+// CSS
+import styles from "./footer.module.css";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2024</p>
+    </footer>
+  );
+}
