539f92d61090cad2a2bd5c5925eedb713f2e70bc
wkauan <kauanmbomfim5@gmail.com>
2024-08-02 00:57:18 -0300

    feat: Criando o sistema de cadastro

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,12 +1,48 @@
 // CSS
+import { useEffect, useState } from "react";
 import styles from "./register.module.css";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
-export function Register() {
+export const Register = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais!");
+
+      return;
+    }
+
+    const res = await createUser(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    setError(authError);
+  }, [authError]);
+
   return (
     <div className={styles.register}>
       <h1>Cadastre-se para postar</h1>
       <p>Crie seu usuário e compartilhe as suas histórias</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* nome */}
         <label>
           <span>Nome:</span>
@@ -15,6 +51,8 @@ export function Register() {
             name="displayName"
             required
             placeholder="Nome do usuario"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
           />
         </label>
 
@@ -26,6 +64,8 @@ export function Register() {
             name="email"
             required
             placeholder="E-mail do usuario"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
 
@@ -37,6 +77,8 @@ export function Register() {
             name="password"
             required
             placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
 
@@ -48,11 +90,20 @@ export function Register() {
             name="confirmPassword"
             required
             placeholder="Confirme a sua senha"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
 
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+
+        {error && <p className="error">{error}</p>}
       </form>
     </div>
   );
-}
+};
