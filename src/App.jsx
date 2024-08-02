6c815fe3857f1ec184a292a7fdc9162f93da1ea1
wkauan <kauanmbomfim5@gmail.com>
2024-08-01 22:16:16 -0300

    fix: Alterando estrutura de rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import "./App.css";
-import { Home } from "./pages/home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
+// CSS
+import "./app.css";
+
+// Pages
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+
+// Components
+import { Footer } from "./components/footer";
+import { Navbar } from "./components/navbar";
+
+export function App() {
   return (
     <>
-      <div className="container">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
-
-export default App;
