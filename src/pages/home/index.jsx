1afc33d93447a379f0acc00194ff2dba88cb04f8
wkauan <kauanmbomfim5@gmail.com>
2024-08-03 00:42:13 -0300

    feat: Iniciando page home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,40 @@
-// CSS
+import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 
+import { useState } from "react";
+
 export const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>MiniBlog - Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrado posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro posts
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
