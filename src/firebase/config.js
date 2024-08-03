7f81c6c86a57ab0cae14bc598d202c31f4bd7df0
wkauan <kauanmbomfim5@gmail.com>
2024-08-02 21:09:40 -0300

    feat: Atualizando inicialização do auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,4 +15,6 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-export { db };
+const auth = getAuth(app);
+
+export { db, auth };
