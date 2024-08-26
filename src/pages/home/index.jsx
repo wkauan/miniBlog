f44f6e3f914ed45c238e100ad3ec7e8602aec2f2
wkauan <kauanmbomfim5@gmail.com>
2024-08-25 22:14:27 -0300

    feat: Finalizando a home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./home.module.css";
 
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { PostDetail } from "../../components/postDetails";
 
 export const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
   };
 
   return (
@@ -25,7 +33,8 @@ export const Home = () => {
       </form>
 
       <div>
-        <h1>Posts...</h1>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrado posts</p>
