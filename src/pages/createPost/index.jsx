72d8d513a03844fa42a67cf365cd7c0e94bcf48d
wkauan <kauanmbomfim5@gmail.com>
2024-08-25 23:58:58 -0300

    refactor: Alterando a classe CSS para um padrão de nome

--- src/pages/createPost/index.jsx
+++ src/pages/createPost/index.jsx
@@ -50,7 +50,7 @@ export const CreatePost = () => {
   };
 
   return (
-    <div className={styles.createPost}>
+    <div className={styles.create_post}>
       <h2>Criar post</h2>
       <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
       <form onSubmit={handleSubmit}>
